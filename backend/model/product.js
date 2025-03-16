66bc7965d55581897582da37b6f1bb7a96116897
unix-waltz <riosraskaa@gmail.com>
2025-03-16 11:21:11 +0700

    Update product model

--- backend/model/product.js
+++ backend/model/product.js
@@ -7,6 +7,7 @@ var Schema = mongoose.Schema;
   title: String,
   target_redirect_url: String,
   target_download_url: String,
+  tipe: String,
   is_delete: { type: Boolean, default: false },
   date: { type: Date, default: Date.now },
 })),
